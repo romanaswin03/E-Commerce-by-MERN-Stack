e7c4b8d6424aaf19c15caf99eb426a54879a2d2a
romanaswin03 <aswinchandru3@gmail.com>
2024-03-19 22:39:07 +0530

    All product database added and you can access and delete it in mongodb compass

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,8 @@
 const epxress = require('express');
-const { getProducts } = require('../controllers/productController');
+const { getProducts, newProduct } = require('../controllers/productController');
 const router = epxress.Router();
 
 router.route('/products').get(getProducts);
+router.route('/product/new').get(newProduct);
 
-module.exports = router
+module.exports = router;
