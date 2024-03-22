562e4fe9dcfdf24de442686653c2cd0f5b1721c7
romanaswin03 <aswinchandru3@gmail.com>
2024-03-22 17:09:13 +0530

    Created another database for user registration form, and successfully connected the router.

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const products = require('./routes/product')
 const errorMiddleware = require('./middlewares/error')
+const auth = require('./routes/auth')
 
 app.use(express.json())
-app.use('/api/v1',products)
+
+app.use('/api/v1',products);
+app.use('/api/v1',auth);
+
 app.use(errorMiddleware)
+
 module.exports = app;
