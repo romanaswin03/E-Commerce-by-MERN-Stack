fd0ce618ec51d4765713e1068c6a3f23fb614462
romanaswin03 <aswinchandru3@gmail.com>
2024-03-23 21:43:35 +0530

    Password reset method and validate password and confirm password and handled duplicate error and handled jsonweb token error and json web token expire error

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { registerUser, loginUser, logoutUser, forgotpassword } = require('../controllers/authController');
+const { registerUser, loginUser, logoutUser, forgotpassword, resetPassword } = require('../controllers/authController');
 const router = express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logoutUser);
 router.route('/password/forgot').post(forgotpassword);
+router.route('/password/reset/:token').post(resetPassword);
 
 module.exports = router;
