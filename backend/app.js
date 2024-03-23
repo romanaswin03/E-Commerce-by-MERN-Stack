2fa9c1f0946d4fe3bfc28505c30372541d7405b1
romanaswin03 <aswinchandru3@gmail.com>
2024-03-23 13:23:25 +0530

    Added cookies and then set that cookies to access products only after user logged in

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,10 @@ const app = express();
 const products = require('./routes/product')
 const errorMiddleware = require('./middlewares/error')
 const auth = require('./routes/auth')
+const cookieParser = require('cookie-parser')
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1',products);
 app.use('/api/v1',auth);
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,9 @@
 const epxress = require('express');
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
 const router = epxress.Router();
+const {isAuthenticateUser} = require('../middlewares/authenticate')
 
-router.route('/products').get(getProducts);
+router.route('/products').get(isAuthenticateUser, getProducts);
 router.route('/product/new').post(newProduct);
 router.route('/product/:id')
                             .get(getSingleProduct)
