29cba54885d7ab1cd636ef293a84c3b712f68fd3
romanaswin03 <aswinchandru3@gmail.com>
2024-03-24 17:41:25 +0530

    Successfully gets the single order details with the order id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 const catchAsyncError = require('../middlewares/catchAsyncError')
 const Order = require('../models/orderModel');
+const ErrorHandler = require('../utils/errorHandler');
 //Create New Order - api/v1/order/new
 exports.newOrder = catchAsyncError(async (req, res, next) =>{
     const {
@@ -23,6 +24,20 @@ exports.newOrder = catchAsyncError(async (req, res, next) =>{
         paidAt: Date.now(),
         user: req.user.id
     })
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+})
+
+
+//Get Single Order - api/v1/order/:id
+exports.getSingleOrder = catchAsyncError(async(req, res, next) =>{
+    const order = await Order.findById(req.params.id).populate('user','name email')
+    if(!order){
+        return next(new ErrorHandler(`Order not found with this id ${req.params.id}`,404))
+    }
 
     res.status(200).json({
         success: true,
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { newOrder } = require('../controllers/orderController');
+const { newOrder, getSingleOrder } = require('../controllers/orderController');
 const { isAuthenticateUser } = require('../middlewares/authenticate');
 const router = express.Router();
 
 router.route('/order/new').post(isAuthenticateUser, newOrder);
+router.route('/order/:id').get(isAuthenticateUser, getSingleOrder);
 
 module.exports = router;
