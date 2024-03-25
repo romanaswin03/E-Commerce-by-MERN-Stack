6c54b58cc83b7f66a99fe8dd51e683f22d94f707
romanaswin03 <aswinchandru3@gmail.com>
2024-03-25 17:45:21 +0530

    Created react app and added header, footer and home element in the react app and connected for the home element

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Home from './components/Home';
+import Footer from './components/layouts/Footer';
+import Header from './components/layouts/Header';
+import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      
+      <Footer />
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/layouts/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="py-1">
+      <p className="text-center text-dark mt-1">
+        Shopping Cart - 2023-2024, All Rights Reserved
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
