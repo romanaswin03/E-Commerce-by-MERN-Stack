874502223b0a45136cfd5426126aa333ab56d2c1
romanaswin03 <aswinchandru3@gmail.com>
2024-03-27 20:59:10 +0530

    Link connected to view product details

--- frontend/src/components/product/productDetail.js
+++ frontend/src/components/product/productDetail.js
@@ -3,6 +3,7 @@ import { getProduct } from "../../actions/productAction";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Loader from '../layouts/Loader';
+import { Carousel } from "react-bootstrap";
 
 export default function productDetail(){
 
@@ -24,8 +25,13 @@ export default function productDetail(){
             <Fragment>
                 <div className="row f-flex justify-content-around">
                     <div className="col-12 col-lg-5 img-fluid" id="product_image">
-                        
-                        <img src="/images/products/3.jpg" alt="sdf" height="500" width="500" />
+                        <Carousel pause="hover">
+                            {product.images && product.images.map(image =>
+                            <Carousel.Item key={image._id}>
+                                <img className="d-block w-100" src={image.image} alt={product.name} height="500" width="500" />
+                            </Carousel.Item>
+                            )}
+                        </Carousel>
                     </div>
 
                     <div className="col-12 col-lg-5 mt-5">
