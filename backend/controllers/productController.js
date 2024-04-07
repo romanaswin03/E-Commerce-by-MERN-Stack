5a07d73324b5e07803e1748acf910940c36f07f1
romanaswin03 <aswinchandru3@gmail.com>
2024-04-07 21:24:07 +0530

    Backend work done for create new product and upload image by the admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -31,15 +31,25 @@ exports.getProducts = catchAsyncError(async(req,res,next)=>{
 })
 //Created product - api/v1/product/new
 exports.newProduct = catchAsyncError(async(req,res,next) =>{
-            
-            req.body.user = req.user.id;
 
-            const product = await Product.create(req.body);
-            res.status(200).json({
-                success: true,
-                message: "Product created sucessfully",
-                product
+        let images = []
+
+        if(req.files.length > 0) {
+            req.files.forEach(file => {
+                let url = `${process.env.BACKEND_URL}/uploads/product/${file.originalname}`
+                images.push({ image: url})
             })
+        }
+        req.body.images = images;
+        
+        req.body.user = req.user.id;
+
+        const product = await Product.create(req.body);
+        res.status(201).json({
+            success: true,
+            message: "Product created sucessfully",
+            product
+        })
 });
 
 
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -12,6 +12,17 @@ const {
     } = require('../controllers/productController');
 const router = epxress.Router();
 const {isAuthenticateUser, authorizedRoles} = require('../middlewares/authenticate')
+const multer = require('multer');
+const path = require('path')
+
+const upload = multer({storage: multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null,path.join(__dirname,'..', 'uploads/product'))
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname)
+    }
+})})
 
 router.route('/products').get(getProducts);
 router.route('/product/:id')
@@ -24,6 +35,6 @@ router.route('/review').put(isAuthenticateUser, createReview)
 router.route('/reviews').get(isAuthenticateUser, getReviews)
 
 //Admin routes
-router.route('/admin/product/new').post(isAuthenticateUser, authorizedRoles('admin'), newProduct);
+router.route('/admin/product/new').post(isAuthenticateUser, authorizedRoles('admin'),upload.array('images'), newProduct);
 router.route('/admin/products').get(isAuthenticateUser, authorizedRoles('admin'), getAdminProducts);
 module.exports = router;
